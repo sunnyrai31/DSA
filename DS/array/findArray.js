166f743450291b32f4325fcfe245cf805d149ddd
sunnyrai <sunnyrai@fico.com>
2022-11-18 00:23:07 +0530

    findArrayItem code corrected

--- DS/array/findArray.js
+++ DS/array/findArray.js
@@ -1,8 +1,10 @@
-export function findArrayItem(arr, item){
-    for (let i =0; i< arr.length; i++){
-        if(arr[i]=== item)
-          return {item: item, index:i};
-
-          return -1;
+function findArrayItem(arr, item) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      return { item: item, index: i };
     }
+  }
+  return -1;
 }
+const res = findArrayItem([1, 2, 3, 4, 5], 5);
+console.log(res);
