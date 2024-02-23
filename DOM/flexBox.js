7ba90bafb880caaf085c77e413c0e9a45a3b4e76
sunnyrai <sunnyrai@fico.com>
2024-02-23 23:41:12 +0530

    interview experience

---
+++ DOM/flexBox.js
@@ -0,0 +1,20 @@
+const html = `<div class="flex-container">
+
+<div class="flex-container-column">
+        <div class="flex-item"> item1</div>
+        <div class="flex-item"> item2</div>
+</div>
+<div class="flex-container-column">
+    <div class="flex-item"> item3</div>
+</div>
+
+<div class="flex-container-column">
+<div class="flex-item"> item4</div>
+<div class="flex-item"> item5</div>
+</div>
+</div>
+`;
+const body = document.querySelector("#flexBoxContainer");
+body.innerHTML = html;
+
+// image.png;
